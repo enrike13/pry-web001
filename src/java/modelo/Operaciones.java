e29c4803dbe14304b93e5a35dd8c186852971162
eencarnacion <eencarnacion@ZRIX-REBUTI5972.sunarp.gob.pe>
2019-07-02 16:44:15 -0500

    Primera version del programa

---
+++ src/java/modelo/Operaciones.java
@@ -0,0 +1,24 @@
+package modelo;
+
+public class Operaciones {
+
+    public static int suma(int x, int y) {
+        return x + y;
+    }
+    public static int resta(int x, int y) {
+        return x - y;
+    }
+    public static int multiplicacion(int x, int y) {
+        return x * y;
+    }
+    public static int divicion(int x, int y) {
+        return x / y;
+    }
+    
+//    public static int operar(int op){
+//        if(op == "suma"){
+//        
+//    }
+//    }
+
+}
